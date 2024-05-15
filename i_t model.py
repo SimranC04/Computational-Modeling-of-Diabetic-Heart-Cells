483f2975c9933ceb8bf71c37c18b1f71714d8bc9
Simran Chandradevan <schb832@aucklanduni.ac.nz>
2024-05-16 09:50:18 +1200

    Added constants from papers

--- i_t model.py
+++ i_t model.py
@@ -13,5 +13,26 @@ Calcium-independent Transient Outward K+ Current (I_t)
     - very simple to pull out and code up
     - look at paper to describe the channel and it will show an equation for it
 """
+""" C o n s t a n t s """
+# Constants from the paper itself 
+g_t = 0.035
+g_t_endo = 0.4647*g_t
+a_endo = 0.583
+b_endo =  0.417
+a_epi = 0.886
+b_epi = 0.114
+K_o = 5.4  
+K_i= 139.2751 
+T = 295
+R = 8314.5  
+F = 96487 
+V = -80.514
+
+
+
+
+
+
+
 
 
