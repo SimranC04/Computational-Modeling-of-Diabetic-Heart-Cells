6fcf8d50c721e19f5e8b1dd078ad28944c24b9cf
Simran Chandradevan <schb832@aucklanduni.ac.nz>
2024-06-10 23:16:09 +1200

    ✅ Added setup_imports file

---
+++ setup_imports.py
@@ -0,0 +1,9 @@
+import os
+import sys 
+
+# python file 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# notebooks
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), 'Python File')))
