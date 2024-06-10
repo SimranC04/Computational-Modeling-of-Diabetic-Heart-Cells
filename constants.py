9f568c4d92a2d1a25a49430a42e4c9440f58d523
Simran Chandradevan <schb832@aucklanduni.ac.nz>
2024-06-10 23:08:27 +1200

    ✅ Added constants file

---
+++ constants.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+# Concentraions 
+Na_i = 1.073519e+01
+K_i = 1.392751e+02
+Ca_i = 7.901351e-05
+
+Na_o = 140.0
+K_o = 5.4
+Ca_o = 1.2
+
+# Constants 
+g_BNa = 8.015e-05
+g_BCa = 3.24e-05
+g_BK = 13.8e-05
+
+#Nernst Stuff
+R = 8.314
+T = 310.15
+F = 96485
+
+# nernst 
+E_Na = (R * T / F) * np.log(Na_o / Na_i) * 1000
+E_K = (R * T / F) * np.log(K_o / K_i) * 1000
+E_CaL = (R * T / (2 * F)) * np.log(Ca_o / Ca_i) * 1000
+
+
